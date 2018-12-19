215a452ceb1378eade094dbfcc26ae576320e874
0xTatsu <nghia@autonomous.nyc>
2018-12-19 16:38:55 +0700

    remove https

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const BASE_API = {
   BASE_URL:
-    process.env.NODE_ENV === 'production' ? 'https://35.247.16.10:8000' : '',
+    process.env.NODE_ENV === 'production' ? 'http://35.247.16.10:8000' : '',
   TIMEOUT: 10000
 };
 
