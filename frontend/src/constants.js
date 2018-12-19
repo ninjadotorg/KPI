f90ffc93dc8db8bdc123ff634b65a6d27e048e9c
0xTatsu <nghia@autonomous.nyc>
2018-12-19 17:09:51 +0700

    use base_url env

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,5 @@
 export const BASE_API = {
-  BASE_URL:
-    process.env.NODE_ENV === 'production' ? 'http://35.247.16.10:8000' : '',
+  BASE_URL: process.env.REACT_APP_BASE_URL, // 'http://35.247.16.10:8000'
   TIMEOUT: 10000
 };
 
