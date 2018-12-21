72a4a5d82e15f4a9bafa40891d33e8c45e8ef1c9
0xBakunawa <trong@autonomous.nyc>
2018-12-21 10:32:22 +0700

    Add keywords

--- restapi/app/bl/user.py
+++ restapi/app/bl/user.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from __future__ import division
+
 from flask import g
 from openpyxl import load_workbook
 from app.models import User
 
 import os
 import hashlib
+import re
+import unicodedata
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -34,4 +39,24 @@ def read_data():
         print(str(ex))
 
     return arr
-    
+
+
+def no_accent_vietnamese(s):
+    s = s.encode('utf-8').decode('utf-8') 
+    s = re.sub(u'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(u'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(u'èéẹẻẽêềếệểễ', 'e', s)
+    s = re.sub(u'ÈÉẸẺẼÊỀẾỆỂỄ', 'E', s)
+    s = re.sub(u'òóọỏõôồốộổỗơờớợởỡ', 'o', s)
+    s = re.sub(u'ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ', 'O', s)
+    s = re.sub(u'ìíịỉĩ', 'i', s)
+    s = re.sub(u'ÌÍỊỈĨ', 'I', s)
+    s = re.sub(u'ùúụủũưừứựửữ', 'u', s)
+    s = re.sub(u'ƯỪỨỰỬỮÙÚỤỦŨ', 'U', s)
+    s = re.sub(u'ỳýỵỷỹ', 'y', s)
+    s = re.sub(u'ỲÝỴỶỸ', 'Y', s)
+    s = re.sub(u'Đ', 'D', s)
+    s = re.sub(u'đ', 'd', s)
+
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
