bde5a83ca4cff3f9b7cbd59abb1b21d5629c731d
Hakawai <vtkimsa@gmail.com>
2019-01-14 11:04:25 +0700

    Change to anonymous

--- frontend/src/models/comments.js
+++ frontend/src/models/comments.js
@@ -20,8 +20,8 @@ class Comments extends Component {
         return (
             <div className="wrapperCommentItem" key={id}>
                 <div className="wrapperUser">
-                    <Avatar alt="User" src={avatar || DefaultAvatar} className="avatar" />
-                    <div className="name">{userName}</div>
+                    <Avatar alt="User" src={DefaultAvatar} className="avatar" />
+                    {/*<div className="name">{userName}</div>*/}
                     <div className="ratedText">rated</div>
                     {<div className="commentRater">
                         <Rater total={5} rating={point} interactive={false}/>
